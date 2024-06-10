b9d680f6dfb08b6741cd77f74254cb9d498fdc06
Saleh764 <salehbanaklech@icloud.com>
2024-06-10 10:02:36 +0300

    Add Generice of repository

---
+++ DAL/Repositories/GenericeRepositories/GenericeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories.GenericeRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.GenericeRepository
+{
+    public class GenericeRepository<T> : IGenericeRepository<T> where T : class
+    {
+        public readonly DbSet<T> _dbSet;
+        public readonly DbContext dbContext;
+    }
+}
---
+++ DAL/Repositories/GenericeRepositories/IGenericeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.GenericeRepositories
+{
+    public interface IGenericeRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T GetById(int id);
+        T Add(T Pilot);
+        T Update(T Pilot);
+        bool Delete(int id);
+        bool Delete(T Pilot);
+    }
+}
